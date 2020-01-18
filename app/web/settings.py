205f9a3ebe129b94e722254e07d93b7b498d3971
John Heerman <john.heerman@agcocorp.com>
2020-01-17 20:23:16 -0600

    Dockerize boilerplate app

--- app/web/settings.py
+++ app/web/settings.py
@@ -68,7 +68,7 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static", "dist"),
 ]
 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # List of finder classes that know how to find static files in
 # various locations.
---
+++ docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+echo 'alias ll="ls -l"' >> ~/.bashrc
+echo 'alias la="ls -A"' >> ~/.bashrc
+echo 'set -o vi' >> ~/.bashrc
+
+STATUS=$(curl --max-time 5 -s -o /dev/null -w '%{http_code}' http://169.254.170.2/v2/metadata/)
+if [ $STATUS -eq 200 ]; then
+    echo "Running in AWS, pull parameters from ssm parameter store"
+    export RUNNING_IN_AWS=1
+else
+    echo "Got $STATUS : Not running in AWS"
+    export RUNNING_IN_AWS=0
+fi
+
+echo "Initializing cron."
+touch /etc/cron.d/django-boilerplate
+env >> /etc/cron.d/django-boilerplate
+echo 'SHELL=/bin/bash' >> /etc/cron.d/django-boilerplate
+cat /etc/cron.d/django-boilerplate-cron >> /etc/cron.d/django-boilerplate
+rm /etc/cron.d/django-boilerplate-cron
+chmod 0644 /etc/cron.d/django-boilerplate
+crontab /etc/cron.d/django-boilerplate
+
+echo "Done intializing cron."
+echo "Django Environment: $DJANGO_ENV"
+echo "Application profile is: $APPLICATION_PROFILE"
+echo "Analyzing data fixtures."
+
+python /home/docker/code/app/manage.py migrate --noinput
+
+#Reset the data each time in the dev environment
+if [ $RUNNING_IN_AWS -eq 0 ]; then
+    echo "Loading fixture data into local environment..."
+    python /home/docker/code/app/manage.py loaddata sites
+fi
+
+python /home/docker/code/app/manage.py collectstatic --noinput
+
+echo "Starting supervisor process monitoring."
+supervisord -n -c /etc/supervisor/supervisord.conf 
+
+echo "Done."
